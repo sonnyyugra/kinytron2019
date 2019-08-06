05d1de3d2f65242200c2dfb3b1daaa5abf864501
Sonny Yugra <sonnyyugra@gmail.com>
2019-08-06 14:45:38 -0400

    add videogames

---
+++ resources/views/videogames/historia_kinytron.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Unity WebGL Player | Modulos Kinytron</title>
+    <link rel="shortcut icon" href="{{asset('videogames/Kinytron_Modulos/TemplateData/favicon.ico')}}">
+    <link rel="stylesheet" href="{{asset('videogames/Kinytron_Modulos/TemplateData/style.css')}}">
+    <script src="{{asset('videogames/Kinytron_Modulos/TemplateData/UnityProgress.js')}}"></script>  
+    <script src="{{asset('videogames/Kinytron_Modulos/Build/UnityLoader.js')}}"></script>
+    <script>
+      var gameInstance = UnityLoader.instantiate("gameContainer", "videogames/Kinytron_Modulos/Build/Kinytron_Modulos.json", {onProgress: UnityProgress});
+    </script>
+  </head>
+  <body>
+    <div class="webgl-content">
+      <div id="gameContainer" style="width: 960px; height: 600px"></div>
+      <div class="footer">
+        <div class="webgl-logo"></div>
+        <div class="fullscreen" onclick="gameInstance.SetFullscreen(1)"></div>
+        <div class="title">Modulos Kinytron</div>
+      </div>
+    </div>
+  </body>
+</html>
