4b351bbb46ce7e04ac9f796d979251cf72b6d036
Sonny Yugra <sonnyyugra@gmail.com>
2019-09-06 13:36:31 -0400

    seeder 34

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -28,7 +28,7 @@ $factory->define(\Kinytron\Answer::class, function (Faker $faker) use ($autoIncr
     return [
         'answer' => $faker->numberBetween(1, 3),
         'question_number' => $autoIncrement->current(),
-        'measurement_id' => 33,
+        'measurement_id' => 34,
     ];
 });
 
