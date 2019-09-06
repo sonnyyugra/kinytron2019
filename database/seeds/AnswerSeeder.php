0e5771974f2efad426bea3261c049eeea8cb5c7d
Sonny Yugra <sonnyyugra@gmail.com>
2019-09-06 13:48:23 -0400

    seeder 31

--- database/seeds/AnswerSeeder.php
+++ database/seeds/AnswerSeeder.php
@@ -11,16 +11,16 @@ class AnswerSeeder extends Seeder
      */
     public function run()
     {
-        // //AUTOESTIMA
-        // for ($i = 98; $i < 113; $i++) {
-        //     factory(\Kinytron\Answer::class, 20)->create(['user_id' => $i]);
-        // }
+        //AUTOESTIMA
+        for ($i = 953; $i < 968; $i++) {
+            factory(\Kinytron\Answer::class, 20)->create(['user_id' => $i]);
+        }
 
         //CLIMA
         //
-        for ($i = 953; $i < 968; $i++) {
-            factory(\Kinytron\Answer::class, 18)->create(['user_id' => $i]);
-        }
+        // for ($i = 953; $i < 968; $i++) {
+        //     factory(\Kinytron\Answer::class, 18)->create(['user_id' => $i]);
+        // }
         //
         //TRABAJO EN EQUIPO
         // for ($i = 953; $i < 968; $i++) {
