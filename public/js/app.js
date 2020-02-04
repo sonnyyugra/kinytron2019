9eafc048bd3ae5b06fa931a36af497eebab8250f
Sonny Yugra <sonnyyugra@gmail.com>
2020-02-04 02:21:48 -0300

    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -51749,8 +51749,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\kinytron2019\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\kinytron2019\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\kinytron\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\kinytron\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
